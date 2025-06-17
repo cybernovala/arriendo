f0295e9f9f4d2f434a87669a5e282fd46a6202ed
cybernovala <bartese823@gmail.com>
2025-06-17 14:44:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file
 from flask_cors import CORS
-from generar_pdf import generar_pdf
-import io
+from generar_pdf import crear_contrato_pdf
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/generar_pdf", methods=["POST"])
-def crear_pdf():
-    try:
-        datos = request.get_json()
-        pdf_bytes = generar_pdf(datos)
-        return send_file(
-            io.BytesIO(pdf_bytes),
-            download_name="arriendo_cybernova.pdf",
-            as_attachment=True,
-            mimetype="application/pdf"
-        )
-    except Exception as e:
-        print("Error al generar PDF:", e)
-        return jsonify({"error": "Error interno del servidor"}), 500
+def generar_pdf():
+    data = request.get_json()
+    pdf_bytes = crear_contrato_pdf(data)
+    return send_file(pdf_bytes, mimetype="application/pdf", download_name="arriendo_cybernova.pdf", as_attachment=True)
