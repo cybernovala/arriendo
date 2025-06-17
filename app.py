e39f5e39d1c4621ccf2b46f9dc85d19a46bcfae9
cybernovala <bartese823@gmail.com>
2025-06-17 14:59:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
-from generar_pdf import crear_contrato_pdf
+from generar_pdf import generar_pdf
+import io
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/generar_pdf", methods=["POST"])
-def generar_pdf():
+def generar():
     data = request.get_json()
-    pdf_bytes = crear_contrato_pdf(data)
-    return send_file(pdf_bytes, mimetype="application/pdf", download_name="arriendo_cybernova.pdf", as_attachment=True)
+    output = generar_pdf(data)
+    return send_file(output, as_attachment=True, download_name="arriendo_cybernova.pdf")
